9bf50b9fea2350bab1111c938f7875f1bb54a4d6
SHIKi <secret>
2025-02-20 00:46:29 +0800

    finish benchmark vector_add kernel

--- 01_vector_add/vector_add.py
+++ 01_vector_add/vector_add.py
@@ -43,3 +43,37 @@ output_torch = x + y
 
 torch.testing.assert_close(output_triton, output_torch)
 
+
+
+@triton.testing.perf_report(
+    triton.testing.Benchmark(
+        x_names=['size'],
+        x_vals=[2**i for i in range(12, 28, 1)],
+        x_log=True,
+        line_arg='provider',
+        line_vals=['triton', 'torch'],
+        line_names=['triton', 'torch'],
+        styles=[('blue', '-'), ('green', '-')],
+        ylabel='GB/s',
+        plot_name='vector-add-performance',
+        args={},
+    )
+)
+def benchmark(size, provider):
+    x = torch.rand(size, device=device)
+    y = torch.rand(size, device=device)
+    
+    quantiles = [0.5, 0.2, 0.8]
+
+    if provider == 'triton':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: add(x, y), quantiles=quantiles)
+
+    if provider == 'torch':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: x + y, quantiles=quantiles)
+
+    gbps = lambda ms: x.numel() * x.element_size() * 3e-9 / (ms * 1e-3)
+
+    return gbps(ms), gbps(max_ms), gbps(min_ms)
+
+
+benchmark.run(print_data=True, save_path='./result')
