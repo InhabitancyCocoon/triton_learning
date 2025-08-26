c3490bdcabbc0ee9bf86471594b763f11bf3af46
shiki <secret>
2025-08-26 17:25:40 +0800

    chore: grouped matmul I dont get the pointer arithmetic

--- 07_grouped_gemm/grouped_gemm.py
+++ 07_grouped_gemm/grouped_gemm.py
@@ -1,25 +1,52 @@
+"""
+Group GEMM
+============================
+This group gemm kernel launches a fixed number of CTA to compute a group
+of gemms. The scheduling is static and we do it on device.
+"""
+
+# Copyright (c) 2023 - 2025 NVIDIA Corporation & Affiliates. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from typing import Optional
 import torch
+
 import triton
 import triton.language as tl
 
+DEVICE = triton.runtime.driver.active.get_active_torch_device()
 
-group_m = [1024, 512, 256, 128]
-group_n = [1024, 512, 256, 128]
-group_k = [1024, 512, 256, 128]
 
-assert len(group_m) == len(group_n)
-assert len(group_k) == len(group_n)
+def is_cuda():
+    return triton.runtime.driver.active.get_current_target().backend == "cuda"
 
-group_size = len(group_m)
 
-group_A, group_B = [], []
+def supports_tma():
+    return is_cuda() and torch.cuda.get_device_capability()[0] >= 9
 
-for i in range(group_size):
-    M, N, K = group_m[i], group_n[i], group_k[i]
-    A = torch.rand((M, K), device='cuda', dtype=torch.bfloat16)
-    B = torch.rand((N, K), device='cuda', dtype=torch.bfloat16)
-    group_A.append(A)
-    group_B.append(B)
+
+def num_sms():
+    if is_cuda():
+        return torch.cuda.get_device_properties("cuda").multi_processor_count
+    return 148
 
 
 @triton.autotune(
@@ -48,101 +75,167 @@ for i in range(group_size):
             'BLOCK_SIZE_K': 32,
             'NUM_SM': 128,
         }),
+        triton.Config({
+            'BLOCK_SIZE_M': 128,
+            'BLOCK_SIZE_N': 128,
+            'BLOCK_SIZE_K': 64,
+            'NUM_SM': num_sms(),
+        }),
+        triton.Config({
+            'BLOCK_SIZE_M': 64,
+            'BLOCK_SIZE_N': 128,
+            'BLOCK_SIZE_K': 64,
+            'NUM_SM': num_sms(),
+        }),
     ],
     key=['group_size'],
 )
 @triton.jit
-def grouped_gemm_kernel(
-    A_ptrs,
-    B_ptrs,
-    C_ptrs,
-    g_sizes,
+def grouped_matmul_kernel(
+    # device tensor of matrices pointers
+    group_a_ptrs,
+    group_b_ptrs,
+    group_c_ptrs,
+    # device tensor of gemm sizes. its shape is [group_size, 3]
+    # dim 0 is group_size, dim 1 is the values of <M, N, K> of each gemm
+    group_gemm_sizes,
+    # device tensor of leading dimension sizes. its shape is [group_size, 3]
+    # dim 0 is group_size, dim 1 is the values of <lda, ldb, ldc> of each gemm
     g_lds,
+    # number of gemms
     group_size,
-    NUM_SUM: tl.constexpr,
+    # number of virtual SM
+    NUM_SM: tl.constexpr,
+    # tile sizes
     BLOCK_SIZE_M: tl.constexpr,
     BLOCK_SIZE_N: tl.constexpr,
-    BLOCK_SIZE_K: tl.constexpr
+    BLOCK_SIZE_K: tl.constexpr,
 ):
     tile_idx = tl.program_id(0)
     last_problem_end = 0
     for g in range(group_size):
-        gm = tl.load(g_sizes + g * 3)
-        gn = tl.load(g_sizes + g * 3 + 1)
-        gk = tl.load(g_sizes + g * 3 + 2)
+        # get the gemm size of the current problem
+        gm = tl.load(group_gemm_sizes + g * 3)
+        gn = tl.load(group_gemm_sizes + g * 3 + 1)
+        gk = tl.load(group_gemm_sizes + g * 3 + 2)
         num_m_tiles = tl.cdiv(gm, BLOCK_SIZE_M)
         num_n_tiles = tl.cdiv(gn, BLOCK_SIZE_N)
         num_tiles = num_m_tiles * num_n_tiles
+        # iterate through the tiles in the current gemm problem
         while (tile_idx >= last_problem_end and tile_idx < last_problem_end + num_tiles):
+            # pick up a tile from the current gemm problem
             k = gk
             lda = tl.load(g_lds + g * 3)
             ldb = tl.load(g_lds + g * 3 + 1)
             ldc = tl.load(g_lds + g * 3 + 2)
-            a_ptr = ?
-            b_ptr = ?
-            c_ptr = ?
-
-            tile_idx_within = tile_idx - last_problem_end
-            tile_m_idx = tile_idx_within // num_n_tiles
-            tile_n_idx = tile_idx_within % num_n_tiles
+            a_ptr = tl.load(group_a_ptrs + g).to(tl.pointer_type(tl.float16))
+            b_ptr = tl.load(group_b_ptrs + g).to(tl.pointer_type(tl.float16))
+            c_ptr = tl.load(group_c_ptrs + g).to(tl.pointer_type(tl.float16))
+            # figure out tile coordinates
+            tile_idx_in_gemm = tile_idx - last_problem_end
+            tile_m_idx = tile_idx_in_gemm // num_n_tiles
+            tile_n_idx = tile_idx_in_gemm % num_n_tiles
 
             # do regular gemm here
-            
+            offs_am = tile_m_idx * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+            offs_bn = tile_n_idx * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+            offs_k = tl.arange(0, BLOCK_SIZE_K)
+            a_ptrs = a_ptr + offs_am[:, None] * lda + offs_k[None, :]
+            b_ptrs = b_ptr + offs_k[:, None] * ldb + offs_bn[None, :]
+            accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+            for kk in range(0, tl.cdiv(k, BLOCK_SIZE_K)):
+                # hint to Triton compiler to do proper loop pipelining
+                tl.multiple_of(a_ptrs, [16, 16])
+                tl.multiple_of(b_ptrs, [16, 16])
+                # assume full tile for now
+                a = tl.load(a_ptrs)
+                b = tl.load(b_ptrs)
+                accumulator += tl.dot(a, b)
+                a_ptrs += BLOCK_SIZE_K
+                b_ptrs += BLOCK_SIZE_K * ldb
+            c = accumulator.to(tl.float16)
+
+            offs_cm = tile_m_idx * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+            offs_cn = tile_n_idx * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+            c_ptrs = c_ptr + ldc * offs_cm[:, None] + offs_cn[None, :]
+
+            # assumes full tile for now
+            tl.store(c_ptrs, c)
+
+            # go to the next tile by advancing NUM_SM
+            tile_idx += NUM_SM
+
+        # get ready to go to the next gemm problem
+        last_problem_end = last_problem_end + num_tiles
 
 
-
-
-
-def grouped_gemm_fn(group_A, group_B):
-    device = torch.device('cuda')
+def group_gemm_fn(group_A, group_B):
     assert len(group_A) == len(group_B)
+    group_size = len(group_A)
 
-    A_ptrs = []
-    B_ptrs = []
-    C_ptrs = []
+    A_addrs = []
+    B_addrs = []
+    C_addrs = []
     g_sizes = []
     g_lds = []
     group_C = []
-
-    for A, B in zip(group_A, group_B):
+    for i in range(group_size):
+        A = group_A[i]
+        B = group_B[i]
         assert A.shape[1] == B.shape[0]
         M, K = A.shape
         K, N = B.shape
-        C = torch.empty((M, N), device=device, dtype=A.dtype)
+        C = torch.empty((M, N), device=DEVICE, dtype=A.dtype)
         group_C.append(C)
-
-        A_ptrs.append(A.data_ptr())
-        B_ptrs.append(B.data_ptr())
-        C_ptrs.append(C.data_ptr())
-
+        A_addrs.append(A.data_ptr())
+        B_addrs.append(B.data_ptr())
+        C_addrs.append(C.data_ptr())
         g_sizes += [M, N, K]
         g_lds += [A.stride(0), B.stride(0), C.stride(0)]
 
-    
-    A_ptrs = torch.tensor(A_ptrs, device=device)
-    B_ptrs = torch.tensor(B_ptrs, device=device)
-    C_ptrs = torch.tensor(C_ptrs, device=device)
-    g_sizes = torch.tensor(g_sizes, dtype=torch.int32, device=device)
-    g_lds = torch.tensor(g_lds, dtype=torch.int32, device=device)
-
-    grid = lambda META: (META['NUM_SUM'], )
-
-    grouped_gemm_kernel[grid](
-        A_ptrs,
-        B_ptrs,
-        C_ptrs,
-        g_sizes,
-        g_lds,
-        group_size
+    # note these are device tensors
+    d_a_ptrs = torch.tensor(A_addrs, device=DEVICE)
+    d_b_ptrs = torch.tensor(B_addrs, device=DEVICE)
+    d_c_ptrs = torch.tensor(C_addrs, device=DEVICE)
+    d_g_sizes = torch.tensor(g_sizes, dtype=torch.int32, device=DEVICE)
+    d_g_lds = torch.tensor(g_lds, dtype=torch.int32, device=DEVICE)
+    # we use a fixed number of CTA, and it's auto-tunable
+    grid = lambda META: (META['NUM_SM'], )
+    grouped_matmul_kernel[grid](
+        d_a_ptrs,
+        d_b_ptrs,
+        d_c_ptrs,
+        d_g_sizes,
+        d_g_lds,
+        group_size,
     )
 
     return group_C
 
 
-
-ref_out = [torch.matmul(A, B) for A, B in zip(group_A, group_B)]
-tri_out = None
-
-
+group_m = [1024, 512, 256, 128]
+group_n = [1024, 512, 256, 128]
+group_k = [1024, 512, 256, 128]
+group_A = []
+group_B = []
+group_B_T = []
+assert len(group_m) == len(group_n)
+assert len(group_n) == len(group_k)
+group_size = len(group_m)
 for i in range(group_size):
-    torch.testing.assert_close(ref_out[i], tri_out[i], atol=1e-2, rtol=0)
+    M = group_m[i]
+    N = group_n[i]
+    K = group_k[i]
+    A = torch.rand((M, K), device=DEVICE, dtype=torch.float16)
+    B = torch.rand((K, N), device=DEVICE, dtype=torch.float16)
+    B_T = B.T.contiguous()
+    group_A.append(A)
+    group_B.append(B)
+    group_B_T.append(B_T)
+
+tri_out = group_gemm_fn(group_A, group_B)
+ref_out = [torch.matmul(a, b) for a, b in zip(group_A, group_B)]
+for i in range(group_size):
+    assert torch.allclose(ref_out[i], tri_out[i], atol=1e-4, rtol=1e-4)
+
+print(f"congratulations! your grouped matmul kernel works !")
