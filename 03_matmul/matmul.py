4833b4d409ae7177f75050a8b4129982bec8ec82
shiki <secret>
2025-10-07 13:39:55 +0800

    chore: rm dtype convert

--- 03_matmul/matmul.py
+++ 03_matmul/matmul.py
@@ -119,9 +119,6 @@ def matmul_kernel(
         a_ptrs += BLOCK_SIZE_K * stride_ak
         b_ptrs += BLOCK_SIZE_K * stride_bk
 
-    # this is not necessary, here I only use this to remind myself.
-    accumulator = accumulator.to(tl.float16)
-
     offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
     offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
     c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
